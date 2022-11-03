75b63b18eb7ca002ca2cc67208aee93d1f46a0ac
Jekliw <Jakub1234554321@outlook.com>
2022-11-03 17:47:19 +0100

    tworzenie mapy
    mapa jest ładowana do stringa ale nie zostaje wyczytana przez cout...

--- snake.cpp
+++ snake.cpp
@@ -8,8 +8,28 @@ using namespace std;
 #define KEY_LEFT 75
 #define KEY_RIGHT 77
 
-void nowagra(){
+char mapgen(int wielkosc){
+    int x,y;
+    x = y = (wielkosc/2)+2;
+    string tab[y];
+    for (int i = 0; i <= y; i++)
+    {
+        tab[0] = tab[0] + "#";
+        cout << tab[0];
+        tab[y - 1] = tab[y - 1] + "#";
+        cout << tab[y - 1];
+    }
+    for (int i = 0; i <= y; i++)
+    {
+        cout << tab[i];
+    }
+}
 
+void nowagra(){
+    cout << "Podaj wielkość mapy (przekątna):"<<endl;
+    int wielkosc;
+    cin >> wielkosc;
+    mapgen(wielkosc);
 }
 
 main(){
