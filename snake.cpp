1ef7b7dc23d57840a7f202fa2116ccc120b23be6
Jekliw <Jakub1234554321@outlook.com>
2022-11-03 17:11:31 +0100

    lekkie zmiany

--- snake.cpp
+++ snake.cpp
@@ -8,34 +8,11 @@ using namespace std;
 #define KEY_LEFT 75
 #define KEY_RIGHT 77
 
+void nowagra(){
+
+}
+
 main(){
-
-    int c = 0;
-    while (1)
-    {
-        c = 0;
-
-        switch ((c = getch()))
-        {
-        case KEY_UP:
-            cout << endl
-                 << "Up" << endl; // key up
-            break;
-        case KEY_DOWN:
-            cout << endl
-                 << "Down" << endl; // key down
-            break;
-        case KEY_LEFT:
-            cout << endl
-                 << "Left" << endl; // key left
-            break;
-        case KEY_RIGHT:
-            cout << endl
-                 << "Right" << endl; // key right
-            break;
-        }
-    }
-
-    cout << "Hello World!";
+    nowagra();
     return 0;
 }
