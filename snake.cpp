64e8c733ac3a293be741f454eab0df41b22109a8
Jekliw <52218527+Jekliw@users.noreply.github.com>
2022-11-04 14:44:45 +0100

    tt
    tt

--- snake.cpp
+++ snake.cpp
@@ -8,22 +8,40 @@ using namespace std;
 #define KEY_LEFT 75
 #define KEY_RIGHT 77
 
-char mapgen(int wielkosc){
-    int x,y;
+int varinput(){
+    int x,y,z=0,c=0;
     x = y = (wielkosc/2)+2;
     char tab[y];
-    for (int i = 0; i <= y; i++)
+    return 0;
+}
+
+void genmap2(int x,y,z; char tab[i];){
+        for (int i = 1; i <= 1; i++)
     {
-        tab[0] = tab[0]+'X';
-        cout << tab[0];
-        tab[y - 1] = tab[y - 1]+'X';
-        cout << tab[y - 1];
-    }
-    for (int i = 0; i <= y; i++)
-    {
-        cout << tab[i];
+        tab[0] = 'X';
+        while(z!=2){
+            cout << tab[0];
+            if(c==x){
+                z=2;
+            }
+            c++;
+        }
+        z = c =0;
+        tab[y - 1] = 'X';
+        while(z!=2){
+            cout << tab[y - 1];
+            if(c==x){
+                z=2;
+            }
+            c++;
+        }
     }
 }
+
+char mapgen(int wielkosc){
+    for(int i = 0; i <= y; i++){
+        tab[i] = 0;
+    }
 
 void nowagra(){
     cout << "Podaj wielkość mapy (przekątna):"<<endl;
