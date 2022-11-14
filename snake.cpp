55334f7aaed96a45664729be423a732c1ee6b991
Jekliw <52218527+Jekliw@users.noreply.github.com>
2022-11-14 11:14:27 +0100

    kolejna zmiana budowy mapy
    dobudować i spradzić

--- snake.cpp
+++ snake.cpp
@@ -1,5 +1,5 @@
 using namespace std;
-/**/
+
 #include <conio.h>
 #include <iostream>
 #include <string>
@@ -11,48 +11,21 @@ using namespace std;
 int control=0;
 
 char mapgen(int wielkosc){
-    string tab[50];
-    for(int i=0; i<=9; i++){
-    tab[i] = "X";
+    int scana=1,mapa=0,sglowa=3,scialo=4,owoc=5;
+    int tab[wielkosc];
+    for(int i=0;i!=wielkosc;i++){
+        tab[i]=0;
     }
-    cout << endl;
-    int y=10;
-    int x=0;
-    for(int i=0; i<=9; i++){
-        x=0;
-        tab[x+y] = "X";
-        x=9;
-        tab[x+y] = "X";
-        int z=2;
-        y=10*z;
-        z++;
+    for(int i=0;i<=y;i++){
+        
     }
-    for(int i=0; i<=9; i++){
-    tab[40+i] = "X";
-    }
-    while(control=1){
-        for(int i=0;i<=50;i++){
-            if(tab[i]!="X"){
-             tab[i]="#";
-            }
-        }
-    }
-    y=0;
-    x=1;
-    for(int i=0;i<=50;i++){
-    cout<<tab[i];
-        if(y==10*x){
-            cout << endl;
-            x++;
-        }
-    }
-    return 0;
 }
 
 void nowagra(){
-    cout << "Podaj wielkość mapy (przekątna):"<<endl;
-    int wielkosc;
-    cin >> wielkosc;
+    cout << "Podaj wielkość mapy:"<<endl;
+    int x,y;
+    cin >> x;
+    y=x;
     mapgen(wielkosc);
 }
 
