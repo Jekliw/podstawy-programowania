a8a3abe3054a9efa997d60204e52bdc2a709a838
Jekliw <52218527+Jekliw@users.noreply.github.com>
2022-11-15 18:42:23 +0100

    Update snake.cpp
    Tworzenie mapy zrobione, ustawianie pozycji gracza oraz lekkie poprawki kodu

--- snake.cpp
+++ snake.cpp
@@ -8,36 +8,44 @@ using namespace std;
 #define KEY_DOWN 80
 #define KEY_LEFT 75
 #define KEY_RIGHT 77
-int control=0;
+const int scana=1,mapa=0,sglowa=3,scialo=4,owoc=5,x=11;
+int z=0;
+int tab[x][x];
 
-char mapgen(int x, int y){
-    int z=0,scana=1,mapa=0,sglowa=3,scialo=4,owoc=5;
-    int tab[x][x];
+int mapgen(int x){
     z=0;
     for(;z!=x;z++){
-        if((z==0) || (z==10)){
+        if((z==0) || (z==x-1)){
             for(int i=0;i!=x;i++){
             tab[z][i]=1;
             } 
         }
-        if((z>0) && (z<10)){
+        if((z>0) && (z<x-1)){
             for(int i=0;i!=x;i++){
             tab[z][0]=1;
-                for(int i=1;i<=9;i++){
+                for(int i=1;i<=x-1;i++){
                     tab[z][i]=0;
                 }
-            tab[z][9]=1;
+            tab[z][x-1]=1;
             }  
         }
     }
+return tab[x][x];
+}
+
+int ppose(){
+    int y;
+    y=(x+1)/2;
+    tab[y][y]=sglowa;
+    return tab[x][x];
 }
 
 void nowagra(){
-    int x=10,y;
-    mapgen(x, y);
+    mapgen(x);
+    ppose();
 }
 
-main(){
+int main(){
     nowagra();
     return 0;
 }
