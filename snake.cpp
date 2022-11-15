f411caf886b8103e5649ab96f7ee9b23c2eb08b8
Jekliw <52218527+Jekliw@users.noreply.github.com>
2022-11-15 12:13:20 +0100

    Update snake.cpp

--- snake.cpp
+++ snake.cpp
@@ -10,23 +10,31 @@ using namespace std;
 #define KEY_RIGHT 77
 int control=0;
 
-char mapgen(int wielkosc){
-    int scana=1,mapa=0,sglowa=3,scialo=4,owoc=5;
-    int tab[wielkosc];
-    for(int i=0;i!=wielkosc;i++){
-        tab[i]=0;
-    }
-    for(int i=0;i<=y;i++){
-        
+char mapgen(int x, int y){
+    int z=0,scana=1,mapa=0,sglowa=3,scialo=4,owoc=5;
+    int tab[x][x];
+    z=0;
+    for(;z!=x;z++){
+        if((z==0) || (z==10)){
+            for(int i=0;i!=x;i++){
+            tab[z][i]=1;
+            } 
+        }
+        if((z>0) && (z<10)){
+            for(int i=0;i!=x;i++){
+            tab[z][0]=1;
+                for(int i=1;i<=9;i++){
+                    tab[z][i]=0;
+                }
+            tab[z][9]=1;
+            }  
+        }
     }
 }
 
 void nowagra(){
-    cout << "Podaj wielkość mapy:"<<endl;
-    int x,y;
-    cin >> x;
-    y=x;
-    mapgen(wielkosc);
+    int x=10,y;
+    mapgen(x, y);
 }
 
 main(){
